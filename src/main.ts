91025b6c222f86eb62ade25bc4155d5fe2d2c6da
heewon Choi <wonbb3313@gmail.com>
2023-07-14 22:37:55 +0900

    reset 후 winston-logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import { transports, format } from 'winston';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+     // let's log errors into its own file
+        new transports.File({
+          filename: `logs/error.log`,
+          level: 'error',
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+        // logging all level
+        new transports.File({
+          filename: `logs/combined.log`,
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+        // we also want to see logs in our console
+        new transports.Console({
+         format: format.combine(
+           format.cli(),
+           format.splat(),
+           format.timestamp(),
+           format.printf((info) => {
+             return `${info.timestamp} ${info.level}: ${info.message}`;
+           }),
+          ),
+      }),
+      ],
+    }),
+  });
   await app.listen(3000);
 }
 bootstrap();
