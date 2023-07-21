a5024ed34d8f7c17add08e70f28c324071af46bf
Kimjipang <91196025+Kimjipang@users.noreply.github.com>
2023-07-21 23:33:01 +0900

    [feat] create user API

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RequestLoggerMiddleware } from './request-logger.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      // dropSchema: true, //서버 재시작 할 때마다 DB 데이터 초기화
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
